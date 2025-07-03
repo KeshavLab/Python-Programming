3c523f5d9dbaa55bff7f8e2de36a66b3b6326c7d
KeshavLab <kolekarkeshav38@gmail.com>
2025-07-03 17:35:09 +0530

    initial

---
+++ Basics/type_casting.py
@@ -0,0 +1,12 @@
+'''✅ 2. Type Casting (Explicit) (Done manually by the programmer)
+Manual conversion of data type by the programmer.
+
+Done using built-in functions: int(), float(), str(), list(), etc.
+
+Used when you want to force a conversion between incompatible types.
+
+🔹 Example:'''
+
+a = "100"
+b = int(a)      # explicitly cast string to int
+print(b + 50)   # Output: 150
---
+++ Basics/type_conversion.py
@@ -0,0 +1,15 @@
+'''✅ 1. Type Conversion (Implicit)  (Done automatically by the system)
+Automatic conversion of data type by Python.
+
+Happens without user intervention.
+
+Python promotes one data type to another to avoid data loss.'''
+
+
+
+x = 10      # int
+y = 2.5     # float
+
+z = x + y   # x is implicitly converted to float
+print(z)    # Output: 12.5
+print(type(z))  # <class 'float'>
