dbfa553b8956cfb8ab68291f9ca43aef69ccb05d
KeshavLab <kolekarkeshav38@gmail.com>
2025-07-04 16:33:33 +0530

    conditionals

---
+++ Strings & Conditionals/conditional_statements/Q1.py
@@ -0,0 +1,10 @@
+# WAP  to take input age  from user and check if user is able to vote or not
+
+
+age=int(input("enter your age :"))
+
+if (age>18):
+    print("you can vote")
+
+else:
+    print("you can not vote")
