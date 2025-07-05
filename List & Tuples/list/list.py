ed3480e8b8b4bb20b51d38510fa7b1def435cc9d
KeshavLab <kolekarkeshav38@gmail.com>
2025-07-05 14:12:04 +0530

    list

---
+++ List & Tuples/list/list.py
@@ -0,0 +1,20 @@
+'''In Python, a list is a built-in data structure that allows you to store multiple items in a single variable. 
+Lists are ordered, mutable, and allow duplicate values.'''
+
+#empty list
+my_list=[]
+print(my_list)
+print(type(my_list))
+print(len(my_list))
+
+#List  with values
+numbers=[2,4,5,68,9]
+print(numbers)
+print(type(numbers))
+print(len(numbers))
+
+# List with mixed data types
+students=["keshav",34,"mumbai",20]
+print(students)
+print(type(students))
+print(len(students))
