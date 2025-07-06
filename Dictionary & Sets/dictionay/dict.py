9108bbf082d882cf61de3cad54eb7e1593ae8d74
KeshavLab <kolekarkeshav38@gmail.com>
2025-07-06 16:05:35 +0530

    tt

---
+++ Dictionary & Sets/dictionay/dict.py
@@ -0,0 +1,14 @@
+'''A dictionary in Python is a collection of key-value pairs. It's unordered 
+(until Python 3.6+ where insertion order is preserved), mutable, and indexed by keys.'''
+
+# Empty dictionary
+my_dict = {}
+
+# Dictionary with data
+student = {
+    "name": "Keshav",
+    "age": 23,
+    "course": "Python",
+    "is_active": True
+}
+print(student)
