ec09c3645a83fbcb88c52e6afb92893a0637c697
KeshavLab <kolekarkeshav38@gmail.com>
2025-07-06 16:40:20 +0530

    neasted

---
+++ Dictionary & Sets/dictionay/neasted.py
@@ -0,0 +1,21 @@
+student={
+    "student1":{
+        "name":"keshav",
+        "age":23,
+        "subject":"java"
+    },
+    "student2" :{
+        "name":"rajesh",
+        "age":23,
+        "subject":"python"
+    },
+    "student3" :{
+        "name":"yogesh",
+        "age":24,
+        "subject":"c++"
+    }
+}
+print(student)
+print(student["student1"])
+print(student["student2"])
+print(student["student3"])
