08c403c68ce49ba224816ec5ec9d07abc87a182a
KeshavLab <kolekarkeshav38@gmail.com>
2025-07-07 17:17:50 +0530

    for loop

---
+++ Loops/For Loop/Q1.py
@@ -0,0 +1,4 @@
+fruits=["apple","grape","banana","orange"]
+
+for fruit in fruits:
+    print(fruit)
---
+++ Loops/For Loop/for_loop.py
@@ -0,0 +1,18 @@
+# Example 1: range(5)
+# Starts from 0 (default), goes up to 4 (5 is not included)
+for i in range(5):
+    print(i)  # Output: 0, 1, 2, 3, 4
+
+print("-----")  # Separator for clarity
+
+# Example 2: range(1, 5)
+# Starts from 1, goes up to 4 (5 is not included)
+for i in range(1, 5):
+    print(i)  # Output: 1, 2, 3, 4
+
+print("-----")  # Separator for clarity
+
+# Example 3: range(1, 10, 2)
+# Starts from 1, goes up to 9 (10 not included), with a step of 2
+for i in range(1, 10, 2):
+    print(i)  # Output: 1, 3, 5, 7, 9
