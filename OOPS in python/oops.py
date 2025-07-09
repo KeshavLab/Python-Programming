9962c959a8debd018b28e1651cb285a3d37d983c
KeshavLab <kolekarkeshav38@gmail.com>
2025-07-09 16:36:44 +0530

    oops

---
+++ OOPS in python/oops.py
@@ -0,0 +1,16 @@
+'''✅ Class:
+A class is a blueprint or template to create objects.
+It defines attributes (variables) and methods (functions) that an object can have.'''
+
+# define the class
+class Car:
+    name="toyota"
+    model="fortuner legender"
+    price=3200000
+
+
+# create the object
+car1=Car()
+print(car1.model)
+print(car1.name)
+print(car1.price)
