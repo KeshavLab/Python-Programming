3f62d13cc1c04f33e6376de6e1306e0ea3b0abb8
KeshavLab <kolekarkeshav38@gmail.com>
2025-07-10 14:24:03 +0530

    oops

---
+++ OOPS in python/class & objects problems/class1.py
@@ -0,0 +1,29 @@
+
+
+class BankAccount:
+
+    # constructor
+    def __init__(self,account_holder,balance):
+        self.account_holder=account_holder
+        self.balance=balance
+
+    # method for deposite
+    def deposite(self,amount):
+        if(amount>0):
+            self.balance=self.balance+amount
+            print(f"Deposited :{amount}")
+        
+
+    # function to show balance 
+    def show_balance(self):
+        print(f"Current balance is :{self.balance}")
+
+
+# object create karana hai
+account1=BankAccount("keshav",20000)
+
+# deposite money
+account1.deposite(1000)
+
+# check balance
+account1.show_balance()
