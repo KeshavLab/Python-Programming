19ae611365f03c4207eca9b1bc6a20091ad8e03f
KeshavLab <kolekarkeshav38@gmail.com>
2025-08-17 14:57:49 +0530

    tt

--- DSA Questions/strings questions/count_vowels.py
+++ DSA Questions/strings questions/count_vowels.py
@@ -1,8 +1,18 @@
-text="keshav"
-vowels='aeiouAEIOU'
+# text="keshav"
+# vowels='aeiouAEIOU'
+# count=0
+
+# for char in text:
+#     if char in vowels:
+#         count=count+1
+# print(f"the count of vowels {text} is {count}")
+
+
+text=str(input("enter your tect :"))
+vowels="aeiouAEIOU"
 count=0
 
-for char in text:
-    if char in vowels:
+for i in text:
+    if i in vowels:
         count=count+1
-print(f"the count of vowels {text} is {count}")
+print(f"the count of vowels is {count}")
