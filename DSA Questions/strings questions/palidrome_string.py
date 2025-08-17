2f3410bfe9ede483edcd48a640cca0b7eaf16156
KeshavLab <kolekarkeshav38@gmail.com>
2025-08-17 15:04:49 +0530

    tt

--- DSA Questions/strings questions/palidrome_string.py
+++ DSA Questions/strings questions/palidrome_string.py
@@ -1,13 +1,23 @@
+# text=input("enter your text :")
+
+# text=text.replace(" ","").lower()
+# '''🧠 In Simple Words:
+# (" ") is the thing you want to remove
+
+# "" is what you want to put instead — in this case, nothing'''
+
+# if text==text[::-1]:
+#     print("string is palidrome ")
+
+# else:
+#     print("it is not palidrome string")
+
 text=input("enter your text :")
 
 text=text.replace(" ","").lower()
-'''🧠 In Simple Words:
-(" ") is the thing you want to remove
-
-"" is what you want to put instead — in this case, nothing'''
 
 if text==text[::-1]:
-    print("string is palidrome ")
+    print(f"your text {text} is palidrome string")
 
 else:
-    print("it is not palidrome string")
+    print("it is not palidrome string ")
