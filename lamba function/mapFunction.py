5ae78009955b225f95b1984fe8aeb1f878c83533
KeshavLab <kolekarkeshav38@gmail.com>
2026-02-20 17:36:19 +0530

    lambada function

---
+++ lamba function/mapFunction.py
@@ -0,0 +1,11 @@
+#📌 What is map()?
+
+# It applies a function to every element in a list.
+
+
+numbers = [1, 2, 3, 4, 5]
+
+
+# Using map() to square each number in the list
+square= list(map(lambda x: x*x, numbers))
+print(square) 
