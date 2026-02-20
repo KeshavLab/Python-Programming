ca992c5ba6230b4b111f75f4db037c497ecf1faf
KeshavLab <kolekarkeshav38@gmail.com>
2026-02-20 17:40:25 +0530

    tt

--- lamba function/mapFunction.py
+++ lamba function/mapFunction.py
@@ -9,3 +9,7 @@ numbers = [1, 2, 3, 4, 5]
 # Using map() to square each number in the list
 square= list(map(lambda x: x*x, numbers))
 print(square) 
+
+
+cube  =list(map(lambda y: y**3, numbers))
+print(cube)
