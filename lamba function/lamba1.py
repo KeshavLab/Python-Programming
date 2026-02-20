cc1fff95555e00ff87198fed4735c4c11aff68dc
KeshavLab <kolekarkeshav38@gmail.com>
2026-02-20 17:29:28 +0530

    tt

---
+++ lamba function/lamba1.py
@@ -0,0 +1,11 @@
+''' A lambda function in Python is a small, anonymous (unnamed) function
+ written in a single line.
+ It is used when you need a simple function for a short period of time.'''
+
+#  Syntax of a lambda function:
+# lambda arguments: expression
+
+# Example of a lambda function that adds two numbers:
+add = lambda x, y: x + y
+
+print(add(5,3))
