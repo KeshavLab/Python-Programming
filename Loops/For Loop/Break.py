67c04592a78b9b67dacf66b359a4fe63355ccb94
KeshavLab <kolekarkeshav38@gmail.com>
2026-07-25 20:52:26 +0530

    tt

--- Loops/For Loop/Break.py
+++ Loops/For Loop/Break.py
@@ -2,3 +2,6 @@ for i in range(5):
     if(i==3):
         break
     print(i)
+
+# Terminates (stops) the loop immediately.
+# The remaining iterations are not executed.
--- Loops/For Loop/Countinue.py
+++ Loops/For Loop/Countinue.py
@@ -3,3 +3,8 @@ for i in range(5):
         continue
 
     print(i)
+
+
+
+# Skips the current iteration and moves to the next iteration
+# The loop continues with the remaining iterations.
