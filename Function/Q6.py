23895a87da523a0d660b05bae35683d0d1d36fff
KeshavLab <kolekarkeshav38@gmail.com>
2026-07-26 16:09:30 +0530

    tt

--- Function/Q6.py
+++ Function/Q6.py
@@ -11,3 +11,14 @@ def fatorial_of_num(num):
 
 num=int(input("enter the number :"))
 fatorial_of_num(num)
+
+
+# using recursion
+
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    return n*factorial(n-1)
+
+n=int(input("enter the number :"))
+print(factorial(n))
